c52d78ea76864418fc0748540c87db7abe442a8c
Sergey Simakov <SaneniRem@yandex.ru>
2025-03-07 14:15:20 +0300

    Добавил context.user_data

--- citybot/city_know_bot.py
+++ citybot/city_know_bot.py
@@ -1,12 +1,21 @@
 from citybot.data import load_cities as ls
 import random 
 
-already_been = []
 cities = ls('cities-russia.txt')
 
 
 def greet_user(update, context):
     update.message.reply_text('Привет, пользователь!\n Сыграем в города?')
+
+
+def get_user_cities(context):
+    if 'already_been' not in context.user_data:
+        context.user_data['already_been'] = []
+    return context.user_data['already_been']
+
+
+def set_user_cities(context, cities_list):
+    context.user_data['already_been'] = cities_list
 
 
 def find_last_char(user_city):
@@ -19,7 +28,8 @@ def find_last_char(user_city):
     return last_char
 
 
-def check_city(user_city):
+def check_city(user_city, context):
+    already_been = get_user_cities(context)
     if user_city in already_been:
         return 1 
     elif user_city not in cities[user_city[0].lower()]:
@@ -27,7 +37,8 @@ def check_city(user_city):
     return 3
 
 
-def check_responce(user_city):
+def check_responce(user_city, context):
+    already_been = get_user_cities(context)
     if len(already_been) > 1:
         last_city = already_been[-1]
         if user_city[0].lower() != find_last_char(last_city.lower()):
@@ -35,7 +46,8 @@ def check_responce(user_city):
     return 5
 
 
-def find_city(last_char):
+def find_city(last_char, context):
+    already_been = get_user_cities(context)
     city = random.choice(cities[last_char])
     if city not in already_been:
         return city 
@@ -44,20 +56,24 @@ def find_city(last_char):
             
 def user_city_message_received(update, context):
     user_city = update.message.text
-    check = check_city(user_city.capitalize())
+    user_id = update.message.from_user.id
+    already_been = get_user_cities(context)
+
+    check = check_city(user_city.capitalize(), context)
     if check == 1:
         update.message.reply_text(f'Город {user_city} уже был!')
     elif check == 2:
         update.message.reply_text('Таких городов в нашей необьятной не имеется!')
     elif check == 3:
-        check = check_responce(user_city)
+        check = check_responce(user_city, context)
         if check == 4:
             update.message.reply_text(f'Не та буква. Последний город: {already_been[-1]}')
         elif check == 5:
-            city = find_city(find_last_char(user_city.lower()))
+            city = find_city(find_last_char(user_city.lower()), context)
             already_been.append(user_city)
             if city:
-                update.message.reply_text(f'{city}! Тебе на {find_last_char(city.lower())}')
+                update.message.reply_text(f'{city}!\nТебе на {find_last_char(city.lower()).upper()}!')
                 already_been.append(city)
             else:
                 update.message.reply_text('Конец')
+            set_user_cities(context, already_been)
