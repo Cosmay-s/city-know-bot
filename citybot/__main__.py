1c7a7e9293e33d9db898c935ba04d946e2917fcf
Sergey Simakov <SaneniRem@yandex.ru>
2025-03-11 10:33:27 +0300

    Решена проблема с зависанием и добавлена дата-время в логи

--- citybot/__main__.py
+++ citybot/__main__.py
@@ -4,8 +4,12 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from dotenv import load_dotenv
 from citybot.city_know_bot import  user_city_message_received, greet_user
 
+time_format = '%Y-%m-%d %H:%M:%S'
 logger = logging.getLogger(__name__)
-logging.basicConfig(filename='bot.log', level=logging.INFO, encoding='utf-8')
+logging.basicConfig(filename='bot.log',
+                    level=logging.INFO,
+                    encoding='utf-8', format='%(asctime)s - %(message)s',
+                    datefmt=time_format)
 load_dotenv()
 token = os.getenv('token')
 
--- citybot/city_know_bot.py
+++ citybot/city_know_bot.py
@@ -1,12 +1,13 @@
 from citybot.data import load_cities as ls
 import random 
 import logging
-logger = logging.getLogger(__name__)
 
+
+logger = logging.getLogger(__name__)
 cities = ls('cities-russia.txt')
 
 
-def greet_user(update, context):
+def greet_user(update, context, user_id):
     update.message.reply_text('Привет, пользователь!\nИграем в города\nНачинай!')
     logger.info(f'Пользователь {user_id}: Начал игру')
 
@@ -49,13 +50,51 @@ def check_responce(user_city, context):
     return 5
 
 
-def find_city(last_char, context):
+def find_city(last_char, context, user_id):
     already_been = get_user_cities(context)
     city = random.choice(cities[last_char])
     if city not in already_been:
         return city
-    return None
+    while city in cities[last_char]:
+        city_cash = []
+        city_cash.append(city)
+        logger.info(f'Пользователь {user_id}: Повторный поиск города для ответа вариант: {city}')
+        if city not in already_been:
+            return city
+        
+        if len(set(cities[last_char])) == len(set(city_cash)):
+            logger.info(f'Пользователь {user_id}: кончились города')
+            logger.info(f'{cities[last_char]}')
+            logger.info(f'{city_cash}')
+            return None
+        city = random.choice(cities[last_char])
 
+
+
+def check_user_responce(update, context, user_city, user_id, already_been):
+    check = check_responce(user_city, context)
+    if check == 4:
+        update.message.reply_text(f'Не та буква. Последний город: {already_been[-1]}')
+        logger.info(f'Пользователь {user_id}: Не та буква. Последний город: {already_been[-1]}')
+    elif check == 5:
+        city = find_city(find_last_char(user_city.lower()), context, user_id)
+        already_been.append(user_city)
+        logger.info(f'Пользователь {user_id}: Ответил город {user_city}')
+        finish_check(update, context, city, already_been, user_id)
+
+    set_user_cities(context, already_been)
+    logger.info(f'Пользователь {user_id}: Обновленный список городов:\n {already_been}\n Городов в списке: {len(already_been)}')
+
+
+def finish_check(update, context, city, already_been, user_id):
+    if city:
+        update.message.reply_text(f'{city}!\nТебе на {find_last_char(city.lower()).upper()}!')
+        already_been.append(city)
+        logger.info(f'Пользователь {user_id}: Получил в ответ {city}')
+      
+    else:
+        update.message.reply_text('Конец')
+        logger.info(f'Пользователь {user_id}: Конец игры')
             
 def user_city_message_received(update, context):
     user_city = update.message.text
@@ -66,23 +105,11 @@ def user_city_message_received(update, context):
     if check == 1:
         update.message.reply_text(f'Город {user_city} уже был!')
         logger.info(f'Пользователь {user_id}: Город {user_city} уже был')
-    elif check == 2:
+        return
+    if check == 2:
         update.message.reply_text('Таких городов в нашей необьятной не имеется!')
         logger.warning(f'Пользователь {user_id}: Город {user_city} не найден в списке')
-    elif check == 3:
-        check = check_responce(user_city, context)
-        if check == 4:
-            update.message.reply_text(f'Не та буква. Последний город: {already_been[-1]}')
-            logger.info(f'Пользователь {user_id}: Не та буква. Последний город: {already_been[-1]}')
-        elif check == 5:
-            city = find_city(find_last_char(user_city.lower()), context)
-            already_been.append(user_city)
-            if city:
-                update.message.reply_text(f'{city}!\nТебе на {find_last_char(city.lower()).upper()}!')
-                already_been.append(city)
-                logger.info(f'Пользователь {user_id}: Получил в ответ {city}')
-            else:
-                update.message.reply_text('Конец')
-                logger.info(f'Пользователь {user_id}: Конец игры')
-            set_user_cities(context, already_been)
-            logger.info(f'Пользователь {user_id}: Обновленный список городов:\n {already_been}\n Городов в списке: {len(already_been)}')
+        return
+    if check == 3:
+        check_user_responce(update, context, user_city, user_id, already_been)
+        
